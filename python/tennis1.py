e4b7d1712220c18d259e2bee2ebb820d3d5c1b54
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-05 21:18:23 +0100

    Introduced a guard clause for advantage player 1.

--- python/tennis1.py
+++ python/tennis1.py
@@ -21,10 +21,10 @@ class TennisGame1:
             return "Deuce"
         if game_tied:
             return "%s-All" % TennisGame1.points_as_text(self.p1points)
+        if (self.p1points >= 4 or self.p2points >= 4) and points_difference == 1:
+            return "Advantage %s" % self.player1Name
         if self.p1points >= 4 or self.p2points >= 4:
-            if points_difference == 1:
-                return "Advantage %s" % self.player1Name
-            elif points_difference == -1:
+            if points_difference == -1:
                 return "Advantage %s" % self.player2Name
             elif points_difference >= 2:
                 return "Win for %s" % self.player1Name
