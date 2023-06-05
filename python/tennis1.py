873c241eaf303e8825d5680afcd9c4a25ca63d03
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-05 17:11:13 +0100

    Improved code formatting.

--- python/tennis1.py
+++ python/tennis1.py
@@ -21,31 +21,31 @@ class TennisGame1:
             return "Deuce"
         if game_tied:
             result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
+                0: "Love-All",
+                1: "Fifteen-All",
+                2: "Thirty-All",
             }[self.p1points]
-        elif (self.p1points>=4 or self.p2points>=4):
-            points_difference = self.p1points-self.p2points
-            if (points_difference==1):
-                result ="Advantage player1"
-            elif (points_difference ==-1):
-                result ="Advantage player2"
-            elif (points_difference>=2):
+        elif (self.p1points >= 4 or self.p2points >= 4):
+            points_difference = self.p1points - self.p2points
+            if (points_difference == 1):
+                result = "Advantage player1"
+            elif (points_difference == -1):
+                result = "Advantage player2"
+            elif (points_difference >= 2):
                 result = "Win for player1"
             else:
-                result ="Win for player2"
+                result = "Win for player2"
         else:
-            for i in range(1,3):
-                if (i==1):
+            for i in range(1, 3):
+                if (i == 1):
                     tempScore = self.p1points
                 else:
-                    result+="-"
+                    result += "-"
                     tempScore = self.p2points
                 result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
+                    0: "Love",
+                    1: "Fifteen",
+                    2: "Thirty",
+                    3: "Forty",
                 }[tempScore]
         return result
