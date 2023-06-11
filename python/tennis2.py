95703d1d2d39ee52e87987877375dabb54020f62
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-11 21:50:54 +0100

    Made similar code even more similar.

--- python/tennis2.py
+++ python/tennis2.py
@@ -29,13 +29,13 @@ class TennisGame2:
         P2res = ""
         if (self.p1points > 0 and self.p2points==0):
             points_to_text = {
+                0: 'Love',
                 1: 'Fifteen',
                 2: 'Thirty',
                 3: 'Forty'
             }
             P1res = points_to_text.get(self.p1points, '')
-
-            P2res = "Love"
+            P2res = points_to_text[self.p2points]
             result = P1res + "-" + 'Love'
         if (self.p2points > 0 and self.p1points==0):
             points_to_text = {
