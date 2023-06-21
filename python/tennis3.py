2c1f208e5f25ac65f65325f3ba2d236494e41eac
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-21 19:48:25 +0100

    Renamed the state variables to be a little clearer.

--- python/tennis3.py
+++ python/tennis3.py
@@ -3,23 +3,23 @@
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
         self.player1_name = player1Name
-        self.p2N = player2Name
+        self.player2_name = player2Name
         self.player1_points = 0
-        self.p2 = 0
+        self.player2_points = 0
 
     def won_point(self, n):
         if n == self.player1_name:
             self.player1_points += 1
         else:
-            self.p2 += 1
+            self.player2_points += 1
 
     def score(self):
-        if (self.player1_points < 4 and self.p2 < 4) and (self.player1_points + self.p2 < 6):
+        if (self.player1_points < 4 and self.player2_points < 4) and (self.player1_points + self.player2_points < 6):
             p = ["Love", "Fifteen", "Thirty", "Forty"]
             s = p[self.player1_points]
-            return s + "-All" if (self.player1_points == self.p2) else s + "-" + p[self.p2]
+            return s + "-All" if (self.player1_points == self.player2_points) else s + "-" + p[self.player2_points]
         else:
-            if (self.player1_points == self.p2):
+            if (self.player1_points == self.player2_points):
                 return "Deuce"
-            s = self.player1_name if self.player1_points > self.p2 else self.p2N
-            return "Advantage " + s if ((self.player1_points - self.p2) * (self.player1_points - self.p2) == 1) else "Win for " + s
+            s = self.player1_name if self.player1_points > self.player2_points else self.player2_name
+            return "Advantage " + s if ((self.player1_points - self.player2_points) * (self.player1_points - self.player2_points) == 1) else "Win for " + s
