19438833bea91b39cec2e40b546cdb7fc283d193
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-21 19:13:53 +0100

    Improved the name of the method.

--- python/tennis3.py
+++ python/tennis3.py
@@ -4,22 +4,22 @@ class TennisGame3:
     def __init__(self, player1Name, player2Name):
         self.player1_name = player1Name
         self.p2N = player2Name
-        self.p1 = 0
+        self.player1_points = 0
         self.p2 = 0
 
     def won_point(self, n):
         if n == self.player1_name:
-            self.p1 += 1
+            self.player1_points += 1
         else:
             self.p2 += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
+        if (self.player1_points < 4 and self.p2 < 4) and (self.player1_points + self.p2 < 6):
             p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+            s = p[self.player1_points]
+            return s + "-All" if (self.player1_points == self.p2) else s + "-" + p[self.p2]
         else:
-            if (self.p1 == self.p2):
+            if (self.player1_points == self.p2):
                 return "Deuce"
-            s = self.player1_name if self.p1 > self.p2 else self.p2N
-            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+            s = self.player1_name if self.player1_points > self.p2 else self.p2N
+            return "Advantage " + s if ((self.player1_points - self.p2) * (self.player1_points - self.p2) == 1) else "Win for " + s
