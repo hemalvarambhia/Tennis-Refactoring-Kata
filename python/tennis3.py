fe9b1236c2346dfd2b3750010c8412047a9a362c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-24 00:22:16 +0100

    Replaced an array with a lookup table to show how points translate to textual scores.

--- python/tennis3.py
+++ python/tennis3.py
@@ -15,7 +15,7 @@ class TennisGame3:
 
     def score(self):
         if (self.player1_points < 4 and self.player2_points < 4) and (self.player1_points + self.player2_points < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
+            p = {0: 'Love', 1: 'Fifteen', 2: 'Thirty', 3: 'Forty'}
             s = p[self.player1_points]
             return s + "-All" if (self.player1_points == self.player2_points) else s + "-" + p[self.player2_points]
         else:
