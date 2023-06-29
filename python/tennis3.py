1156e24d393ab0a0ea8c753bae91a7e9b8330ca3
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-29 12:04:13 +0100

    Improve code formatting.

--- python/tennis3.py
+++ python/tennis3.py
@@ -17,12 +17,12 @@ class TennisGame3:
         if (self.player1_points < 4 and self.player2_points < 4) and (self.player1_points + self.player2_points < 6):
             points_to_text = {0: 'Love', 1: 'Fifteen', 2: 'Thirty', 3: 'Forty'}
             s = points_to_text[self.player1_points]
-            if (self.player1_points == self.player2_points):
+            if self.player1_points == self.player2_points:
                 return s + "-All"
             else:
                 return s + "-" + points_to_text[self.player2_points]
         else:
-            if (self.player1_points == self.player2_points):
+            if self.player1_points == self.player2_points:
                 return "Deuce"
             s = self.player1_name if self.player1_points > self.player2_points else self.player2_name
             return "Advantage " + s if ((self.player1_points - self.player2_points) * (self.player1_points - self.player2_points) == 1) else "Win for " + s
