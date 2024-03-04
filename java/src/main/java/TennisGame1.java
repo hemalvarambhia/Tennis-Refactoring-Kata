39e376d2fca35f35b4cdd429cf78f6f80d78ffd1
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-04 21:31:49 +0000

    refactor: replaced hard coded string with Java format that takes the player name.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -43,7 +43,7 @@ public class TennisGame1 implements TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
+            if (minusResult==1) score = String.format("Advantage %s", player1Name);
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
