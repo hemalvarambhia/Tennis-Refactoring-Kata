0b6980151b769993c07b13faeddcbda26954ec88
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-04 21:42:29 +0000

    refactor: we can use the String format method here, too, to remove a hard coded string, and push a detail up the callstack.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -44,7 +44,7 @@ public class TennisGame1 implements TennisGame {
         {
             int minusResult = m_score1-m_score2;
             if (minusResult==1) score = String.format("Advantage %s", player1Name);
-            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult ==-1) score = String.format("Advantage %s", player2Name);
             else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
         }
