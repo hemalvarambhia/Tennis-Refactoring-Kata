bcb017fc2df17aa126ec513441541b2478979814
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-08 20:18:15 +0000

    refactor: made the predicate explicit for player 2 winning explicit.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -27,7 +27,7 @@ public class TennisGame1 implements TennisGame {
         if (player1Points >=4 || player2Points >=4)
         {
             if (player1Won()) return String.format("Win for %s", player1Name);
-            else return String.format("Win for %s", player2Name);
+            else if(pointsDifference() <= -2) return String.format("Win for %s", player2Name);
         }
 
         return String.format("%s-%s", runningScore(player1Points), runningScore(player2Points));
