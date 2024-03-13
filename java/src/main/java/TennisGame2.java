f11e0f8d17496ebb3bb96cb30d5f1bc3417a93f4
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-13 19:11:22 +0000

    refactor: reduced the scope of some fields.

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -4,8 +4,8 @@ public class TennisGame2 implements TennisGame
     public int P1point = 0;
     public int P2point = 0;
     
-    public String P1res = "";
-    public String P2res = "";
+    private String P1res = "";
+    private String P2res = "";
     private String player1Name;
     private String player2Name;
 
