c66ad26e813bc236f3ff39916933393511ef664c
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-15 16:29:54 +0000

    refactor: get score for 3 points from Map

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -26,7 +26,7 @@ public class TennisGame2 implements TennisGame
             if (P1point==2)
                 P1res = POINTS_TO_RUNNING_SCORE.get(P1point);
             if (P1point==3)
-                P1res = "Forty";
+                P1res = POINTS_TO_RUNNING_SCORE.get(P1point);
             
             P2res = "Love";
             score = P1res + "-" + P2res;
