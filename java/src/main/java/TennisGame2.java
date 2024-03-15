f15ca32ed554dca8c761f68add1161b7eee216ee
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-15 16:17:38 +0000

    refactor: placed hard-coded running score with getting it from a Map.

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -20,8 +20,9 @@ public class TennisGame2 implements TennisGame
         
         if (P1point > 0 && P2point==0)
         {
-            if (P1point==1)
-                P1res = "Fifteen";
+            if (P1point==1) {
+                P1res = POINTS_TO_RUNNING_SCORE.get(P1point);
+            }
             if (P1point==2)
                 P1res = "Thirty";
             if (P1point==3)
