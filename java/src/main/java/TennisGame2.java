822148902b48247ff8935d8d852df51739cd6f53
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-22 08:03:16 +0000

    refactor: removed obsolete code and improved some code style.

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -3,8 +3,6 @@ import java.util.Map;
 public class TennisGame2 implements TennisGame
 {
     public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
         player1 = new TennisPlayer(player1Name);
         player2 = new TennisPlayer(player2Name);
     }
@@ -38,16 +36,14 @@ public class TennisGame2 implements TennisGame
     }
 
     public void wonPoint(String player) {
-        if (player == player1.getName()) {
+        if (player.equals(player1.getName())) {
             player1.wonPoint();
         } else {
             player2.wonPoint();
         }
     }
 
-    private String player1Name;
     private TennisPlayer player1;
-    private String player2Name;
 
     private TennisPlayer player2;
 
