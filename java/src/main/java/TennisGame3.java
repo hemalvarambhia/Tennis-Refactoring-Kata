160954a4671bebaf358690b055c8972e18b2906d
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-26 22:51:16 +0000

    refactor: simplified an expression.

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -13,7 +13,7 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        if ((player1Points > 3) || (player2Points > 3) || (player1Points + player2Points >= 6)) {
+        if ((player1Points > 3 || player2Points > 3) || (player1Points + player2Points >= 6)) {
             if (scoresAreEqual())
                 return "Deuce";
             String leadingPlayer = getLeadingPlayer();
