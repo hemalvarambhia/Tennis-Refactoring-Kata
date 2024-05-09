d18d5e39e5d250e67145994e08cce7e6705392e0
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-05-09 22:41:27 +0100

    refactor: migrated the logic to a more sensible place.

--- java/src/main/java/TennisGame4.java
+++ java/src/main/java/TennisGame4.java
@@ -44,6 +44,9 @@ public class TennisGame4 implements TennisGame {
         if (receiverHasAdvantage())
             return String.format("Advantage %s", getReceiver());
 
+        if(serverHasWon())
+            return new TennisResult("Win for " + getServer(), "").format();
+
         ResultProvider startingResult = new DefaultResult(this);
         ResultProvider advantageReceiver = new AdvantageReceiver(this, startingResult);
         ResultProvider advantageServer = new AdvantageServer(this, advantageReceiver);
@@ -69,7 +72,7 @@ public class TennisGame4 implements TennisGame {
         return servingPlayer.hasBeaten(receivingPlayer);
     }
 
-    boolean isDeuce() {
+    private boolean isDeuce() {
         return servingPlayer.getPoints() >=3 && receivingPlayer.getPoints() >=3 && servingPlayer.pointsDifference(receivingPlayer) == 0;
     }
 }
@@ -110,18 +113,14 @@ class Deuce implements ResultProvider {
 }
 
 class GameServer implements ResultProvider {
-    private final TennisGame4 game;
     private final ResultProvider nextResult;
 
     public GameServer(TennisGame4 game, ResultProvider nextResult) {
-        this.game = game;
         this.nextResult = nextResult;
     }
 
     @Override
     public TennisResult getResult() {
-        if (game.serverHasWon())
-            return new TennisResult("Win for " + game.getServer(), "");
         return this.nextResult.getResult();
     }
 }
