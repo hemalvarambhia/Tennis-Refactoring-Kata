ce5aadd6c93f6d650293eee972e1c5043a81772b
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-05-26 21:27:52 +0100

    refactor: copied the points part into an and statement so it could become a separate guard clause.

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -31,7 +31,7 @@ class TennisGame1
           2 => "Thirty-All",
       }.fetch(@p1points, "Deuce")
     elsif @p1points >= 4 or @p2points >=4
-      if points_difference == 1
+      if (@p1points >= 4 or @p2points >=4) && points_difference == 1
         return "Advantage #{@player1Name}"
       elsif points_difference == -1
         return "Advantage #{@player2Name}"
