77c12b4914cb417efdedc2899d84d8c15b0a1d87
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-05-29 20:05:20 +0100

    refactor: copied the logic for winning against the opponent to Tennis Player to enhance encapsulation.

--- ruby/tennis_player.rb
+++ ruby/tennis_player.rb
@@ -12,6 +12,11 @@ class TennisPlayer
     @points += 1
   end
 
+  def has_won_against?(opponent:)
+    (points >= 4 or opponent.points >= 4) &&
+      points_difference_over(opponent: opponent) >= 2
+  end
+
   def has_advantage_over?(opponent:)
     (points >= 4 or opponent.points >= 4) &&
       points_difference_over(opponent: opponent) == 1
