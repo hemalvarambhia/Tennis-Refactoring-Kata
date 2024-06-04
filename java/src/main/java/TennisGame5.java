620106fa2f155c878b19fc21a763eee3c4535378
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-06-04 22:07:12 +0100

    refactor: Make Similar Code Even More Similar.

--- java/src/main/java/TennisGame5.java
+++ java/src/main/java/TennisGame5.java
@@ -47,9 +47,9 @@ public class TennisGame5 implements TennisGame {
         lookup.put(Map.entry(3, 0), String.format("%s-%s", toRunningScore(p1), toRunningScore(p2)));
         lookup.put(Map.entry(3, 1), String.format("%s-%s", toRunningScore(p1), toRunningScore(p2)));
         lookup.put(Map.entry(3, 2), String.format("%s-%s", toRunningScore(p1), toRunningScore(p2)));
-        lookup.put(Map.entry(0, 0), String.format("%s", "Love-All"));
-        lookup.put(Map.entry(1, 1), String.format("%s", "Fifteen-All"));
-        lookup.put(Map.entry(2, 2), String.format("%s", "Thirty-All"));
+        lookup.put(Map.entry(0, 0), String.format("%s-All", "Love"));
+        lookup.put(Map.entry(1, 1), String.format("%s-All", "Fifteen"));
+        lookup.put(Map.entry(2, 2), String.format("%s-All", "Thirty"));
         lookup.put(Map.entry(4, 3), "Advantage player1");
         lookup.put(Map.entry(3, 4), "Advantage player2");
         lookup.put(Map.entry(4, 0), "Win for player1");
