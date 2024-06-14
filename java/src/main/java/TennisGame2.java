dd59c10c548a477f2d94fb17b3830139202edf15
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-06-14 23:00:09 +0100

    refactor: Make Similar Code Even More Similar.

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -8,7 +8,7 @@ public class TennisGame2 implements TennisGame
     public String getScore(){
         if (isDeuce()) return "Deuce";
         if (player1.hasAdvantageOver(player2)) return advantage(player1);
-        if (player2.hasAdvantageOver((player1))) return String.format("Advantage %s", player2.getName());
+        if (player2.hasAdvantageOver((player1))) return advantage(player2);
         if (player1.hasBeaten(player2)) return String.format("Win for %s", player1.getName());
         if (player2.hasBeaten(player1)) return String.format("Win for %s", player2.getName());
 
