9277aafc17fd4ee8ac50d4bbbd4a1d4d3396d736
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-06-18 22:39:58 +0100

    refactor: Make Similar Code Even More Similar.

--- java/src/main/java/TennisGame5.java
+++ java/src/main/java/TennisGame5.java
@@ -21,9 +21,9 @@ public class TennisGame5 implements TennisGame {
     public String getScore() {
         if(player1.getPoints() > 16 || player2.getPoints() > 16) throw new IllegalArgumentException("Invalid score.");
         if(isDeuce()) return "Deuce";
-        if(player1.hasAdvantageOver(player2)) return String.format("Advantage %s", player1);
+        if(player1.hasAdvantageOver(player2)) return advantage(player1);
         if(player1.hasBeaten(player2)) return String.format("Win for %s", player1);
-        if(player2.hasAdvantageOver(player1)) return String.format("Advantage %s", player2);
+        if(player2.hasAdvantageOver(player1)) return advantage(player2);
         if(player2.hasBeaten(player1)) return String.format("Win for %s", player2);
 
         if(scoresAreTied()) return String.format("%s-All", player1.runningScore());
@@ -40,5 +40,9 @@ public class TennisGame5 implements TennisGame {
 
     private int pointsDifference() {
         return player1.pointsDifference(player2);
+    }
+
+    private static String advantage(TennisPlayer player) {
+        return  String.format("Advantage %s", player);
     }
 }
