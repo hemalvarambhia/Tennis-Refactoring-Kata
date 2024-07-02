4b058dcb670767eb933f1c575b82f6e5aa0925c2
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-07-02 22:16:25 +0100

    refactor: improved code formatting.

--- java/src/main/java/TennisGame6.java
+++ java/src/main/java/TennisGame6.java
@@ -37,9 +37,7 @@ public class TennisGame6 implements TennisGame {
         if (player1HasWon()) { return won(player1Name); }
         if (player2HasWon()) { return won(player2Name); }
 
-        if(scoresAreTied()) {
-            return String.format("%s-All", runningScore(player1Score));
-        }
+        if(scoresAreTied()) { return String.format("%s-All", runningScore(player1Score)); }
         return String.format("%s-%s", runningScore(player1Score), runningScore(player2Score));
     }
 
